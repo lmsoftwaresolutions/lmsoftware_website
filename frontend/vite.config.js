7ba571fd86bb856cffd8644f99cc337ef84870e7
lmsoftwaresolutions <lmsoftwaresolutions9@gmail.com>
2026-02-18 20:55:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://localhost1:8000",
         changeOrigin: true,
       },
     },
